72ce65a0afbd978c344fa9ea320954b9957fb36d
sukalov <matvey10@gmail.com>
2024-10-20 00:59:12 +0300

    add telegram client and methods necessary for interaction. vid3

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"addi/clients/telegram"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	token := mustToken()
+	fmt.Println("Token: ", token)
+
+	tgClient := telegram.New("host", token)
+	fmt.Println(tgClient)
+
+	// fetcher = fetcher.New()
+
+	// processor = processor.New()
+
+	// consumer.Start(fetcher, processor)
+
+	fmt.Println("Hello, World!")
+}
+
+func mustToken() string {
+	token := flag.String("tg-bot-token", "", "token for access to telegram bot")
+
+	flag.Parse()
+	if *token == "" {
+		log.Fatal()
+	}
+
+	return *token
 }
